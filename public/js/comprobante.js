bcb2daf56967a6ef1e68b92af2ee1ec4ef9bf2f4
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-06-09 15:53:15 -0500

    pages

---
+++ public/js/comprobante.js
@@ -0,0 +1,5 @@
+let volver= document.querySelector("#botonv");
+
+volver.addEventListener('click',()=>{
+    setTimeout(() => { window.location.href="../pages/inicio.html"}, 1800);
+})
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
