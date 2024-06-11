c6b3e7c2c333d0288a62840c6bd02dfcd0269662
nioclandramer <161773056+nioclandramer@users.noreply.github.com>
2024-06-11 11:45:10 -0500

    a

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState } from 'react';
+import CarroComponent from '../public/components/carroComponent.jsx';
 import './App.css';
-import logo from './logo.svg';
+
+export const carroContext = React.createContext();
 
 function App() {
+  const [carro,setCarro]=useState(null);
+
+  const llenaCarro = ()=> {
+    setCarro({
+      name:''
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <carroContext.Provider value={carro}>
+      <div>
+        <CarroComponent/>
+      </div>
+    </carroContext.Provider>
   );
 }
 
